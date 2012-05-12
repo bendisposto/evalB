81d57683bddd0669fb46f9f463b5319a187a8301
Jens Bendisposto <jens@bendisposto.de>
2012-05-12 19:18:18 +0200

    ensure to kill processes when undeploying

--- src/main/java/de/prob/config/ServletContextListener.java
+++ src/main/java/de/prob/config/ServletContextListener.java
@@ -1,18 +1,29 @@
 package de.prob.config;
 
+import java.util.Set;
+
+import javax.servlet.ServletContextEvent;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
+import de.prob.Main;
 import de.prob.MainModule;
 import de.prob.web.WebModule;
 
 public class ServletContextListener extends GuiceServletContextListener {
-	
-	
+
 	@Override
 	protected Injector getInjector() {
 		return Guice.createInjector(new WebModule(), new MainModule());
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		Set<Process> keySet = Main.processes.keySet();
+		for (Process process : keySet) {
+			process.destroy();
+		}
 	}
 
 }

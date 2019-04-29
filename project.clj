ac52e1ccb6b82c5fadbeda642f4d661ce89a4546
David Schneider <david.schneider@bivab.de>
2019-04-29 21:10:27 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject evalback "0.1.0-SNAPSHOT"
+(defproject evalback "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-ring "0.9.3"]]
+  :plugins [[lein-ring "0.12.5"]]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.1.3"]
-                 [org.clojure/data.json "0.2.5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [compojure "1.6.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.async "0.4.490"]
                  [de.hhu.stups/de.prob2.kernel "3.2.3"]
                  [hiccup "1.0.5"]
-                 [ring/ring-core "1.2.1"]
-                 [ring-server "0.3.1"]]
+                 [ring/ring-core "1.7.1"]
+                 ]
   :main evalback.core
   :ring {:handler evalback.core/handler
          :init evalback.core/init
-         ;; :nrepl {:start? true :port 6000}
          :destroy evalback.core/destroy
          :uberwar-name "evalB.war"}
   :aot :all)
